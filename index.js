fd2563e7967c697262364ec7fbf6682cc00df2ea
Yulia-kom <85898864+Yulia-kom@users.noreply.github.com>
2021-10-03 16:27:18 +0300

    copleded 3

--- index.js
+++ index.js
@@ -22,14 +22,66 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
 // 70 === 01:10
 
-function appDateMin(min) {
-  const hours = Math.floor(min / 60);
-  const mins = min % 60;
-  const modifyHours = String(hours).padStart(2, 0);
-  const modifyMins = String(mins).padStart(2, 0);
+// function appDateMin(min) {
+//   const hours = Math.floor(min / 60);
+//   const mins = min % 60;
+//   const modifyHours = String(hours).padStart(2, 0);
+//   const modifyMins = String(mins).padStart(2, 0);
 
-  //   console.log(`Time : ${modifyHours}:${modifyMins}`);
-  return `Time : ${modifyHours}:${modifyMins}`;
+//   //   console.log(`Time : ${modifyHours}:${modifyMins}`);
+//   return `Time : ${modifyHours}:${modifyMins}`;
+// }
+
+// console.log(appDateMin(70));
+
+//4. Напишите код, который будет спрашивать 
+//логин с помощью prompt и логировать результат 
+//в консоль браузера 
+//Если посетитель вводит "Админ",
+//то prompt запрашивает пароль. 
+//Если ничего не ввели или нажата клавиша Esc 
+//вывести стороку "Отменено" 
+//В противном случае вывксти строку "Я вас не знаю" 
+//Пароль проверять так: 
+//Если введён пароль "Я главный", 
+//то вывести строку "Здравствуйте!" 
+//иначе выводить строку "Неверный пароль!" 
+
+const loginInput = prompt("Введите свой логин");
+
+// if (loginInput === "Админ") {
+//     const passwordInput = prompt("Ведите свой пароль");
+//     if (passwordInput === "Я главный") {
+//         console.log("Здравствуйте!");
+//     } else if (passwordInput === null) {
+//         console.log("Отменено");
+//     } else {
+//         console.log("Неверный пароль!");
+//     }
+
+// } else if (loginInput === null) {
+//     console.log("Отменено");
+// } else {
+//     console.log("Я вас не знаю");
+// }
+ 
+switch (loginInput) {
+    case "Админ":
+        const passwordInput = prompt("Ведите свой пароль");
+        switch (passwordInput) {
+            case "Я главный":
+                console.log("Здравствуйте!");
+                break;
+            case null:
+                console.log("Отменено");
+                break;
+            default:
+             console.log("Неверный пароль!");   
+        }
+        break;
+    case null:
+        console.log("Отменено");
+        break;
+    default:
+        console.log("Я вас не знаю");
 }
-
-console.log(appDateMin(70));
